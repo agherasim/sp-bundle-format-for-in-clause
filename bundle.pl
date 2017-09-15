e0a5ce033d1d35974c3ae411d8be962f548708ae
Andrei Gherasim <andrei.gherasim@fashiondays.ro>
2017-09-15 10:49:29 +0300

    Fixing identation

--- bundle.pl
+++ bundle.pl
@@ -2,12 +2,12 @@
 use utf8;
 
 BEGIN:
-	sub trim {
-   		return $_[0] =~ s/^\s+|\s+$|\n+$//rg;
-	}
+sub trim {
+	return $_[0] =~ s/^\s+|\s+$|\n+$//rg;
+}
 
 BEGIN:
-	$l_count = 0;
+$l_count = 0;
 
 while (my $line = <STDIN>) { 
 	$line = trim($line);
@@ -16,10 +16,10 @@ while (my $line = <STDIN>) {
 	} else {
 		$line =~ s/^([\w\W]+)/,\"$1\"/g;
 	}
-    
-    $l_count ++;
-    
-  	print STDOUT $line;
+
+	$l_count ++;
+
+	print STDOUT $line;
 }
 
 END:
