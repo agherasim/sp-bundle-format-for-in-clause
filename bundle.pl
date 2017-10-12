fdd6266e7fda4664849ef787deee3900d7c57b79
Andrei Gherasim <andrei.gherasim@fashiondays.ro>
2017-10-12 10:52:29 +0300

    Fixing bug when input has carriage-return (\r); distinction between wrapping alphanum and num only lines

--- bundle.pl
+++ bundle.pl
@@ -3,23 +3,27 @@ use utf8;
 
 BEGIN:
 sub trim {
-	return $_[0] =~ s/^\s+|\s+$|\n+$//rg;
+	return $_[0] =~ s/^\s+|\s+$//rg;
 }
 
 BEGIN:
 $l_count = 0;
 
-while (my $line = <STDIN>) { 
-	$line = trim($line);
-	if ($l_count == 0) {
-		$line =~ s/^([\w\W]+)/\"$1\"/g;
-	} else {
-		$line =~ s/^([\w\W]+)/,\"$1\"/g;
+while (my $input = <STDIN>) {
+	for my $line (split(/\r|\n/, $input)) { 
+		$line = trim($line);
+ 
+		$wrapper = $line =~ /\D+/ ? '"' : '';
+		if ($l_count == 0) {
+			$line =~ s/^([\w\W]+)/$wrapper$1$wrapper/g;
+		} else {
+			$line =~ s/^([\w\W]+)/,$wrapper$1$wrapper/g;
+		}
+
+		$l_count ++;
+
+		print STDOUT $line;
 	}
-
-	$l_count ++;
-
-	print STDOUT $line;
 }
 
 END:
