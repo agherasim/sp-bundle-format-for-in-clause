a629a10d47e5779a85383835e38d6ea178ff3763
Andrei Gherasim <andrei.gherasim@fashiondays.ro>
2017-10-12 10:55:37 +0300

    Fixing indentation

--- bundle.pl
+++ bundle.pl
@@ -3,27 +3,27 @@ use utf8;
 
 BEGIN:
 sub trim {
-	return $_[0] =~ s/^\s+|\s+$//rg;
+  return $_[0] =~ s/^\s+|\s+$//rg;
 }
 
 BEGIN:
 $l_count = 0;
 
 while (my $input = <STDIN>) {
-	for my $line (split(/\r|\n/, $input)) { 
-		$line = trim($line);
- 
-		$wrapper = $line =~ /\D+/ ? '"' : '';
-		if ($l_count == 0) {
-			$line =~ s/^([\w\W]+)/$wrapper$1$wrapper/g;
-		} else {
-			$line =~ s/^([\w\W]+)/,$wrapper$1$wrapper/g;
-		}
+  for my $line (split(/\r|\n/, $input)) { 
+    $line = trim($line);
 
-		$l_count ++;
+    $wrapper = $line =~ /\D+/ ? '"' : '';
+    if ($l_count == 0) {
+      $line =~ s/^([\w\W]+)/$wrapper$1$wrapper/g;
+    } else {
+      $line =~ s/^([\w\W]+)/,$wrapper$1$wrapper/g;
+    }
 
-		print STDOUT $line;
-	}
+    $l_count ++;
+
+    print STDOUT $line;
+  }
 }
 
 END:
